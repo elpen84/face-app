5817dd344c10ce1529a642e5f3addf16d768b840
elpen84 <elplist@gmail.com>
2021-05-16 15:29:18 -0700

    Update signin.js

--- src/Components/Signin/signin.js
+++ src/Components/Signin/signin.js
@@ -1,6 +1,41 @@
+import { render } from "@testing-library/react";
 import React from "react";
 
-const Signin = ({ onRouteChange }) => {
+
+
+class Signin extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      signInEmail: '',
+      signInPassword:''
+    }
+  }
+
+  onEmailChange = (event) => {
+    this.setState({signInEmail: event.target.value})
+  }
+
+  onPasswordChange = (event) => {
+    this.setState({signInPassword: event.target.value})
+  }
+
+  onSubmitSignIn = () => {
+    fetch('http://localhost:3000/signin', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        email: this.state.signInEmail,
+        password: this.state.signInPassword
+      })
+
+    })
+    this.props.onRouteChange('home')
+  }
+
+  render() {
+  const { onRouteChange } = this.props;
+ 
   return (
     <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
       <main className="pa4 black-80">
@@ -12,6 +47,7 @@ const Signin = ({ onRouteChange }) => {
                 Email
               </label>
               <input
+                onChange={this.onEmailChange}
                 className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
                 type="email"
                 name="email-address"
@@ -23,6 +59,7 @@ const Signin = ({ onRouteChange }) => {
                 Password
               </label>
               <input
+                onChange={this.onPasswordChange}
                 className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
                 type="password"
                 name="password"
@@ -32,7 +69,7 @@ const Signin = ({ onRouteChange }) => {
           </fieldset>
           <div className="">
             <input
-              onClick={() => onRouteChange("home")}
+              onClick={this.onSubmitSignIn}
               className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
               type="submit"
               value="Sign in"
@@ -50,6 +87,8 @@ const Signin = ({ onRouteChange }) => {
       </main>
     </article>
   );
+
+ } 
 };
 
 export default Signin;
